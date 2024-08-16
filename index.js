59363d3b1e58128ec6a5a3e73a97c0c414e76a18
dheerajpradi <dheerajpradi@gmail.com>
2024-08-16 05:11:23 +0000

    graph

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Start your es6 scripts here
+import express from 'express';
+import resolvers from './resolvers';
+import schema from './schema';
+import {graphqlHTTP} from 'express-graphql'
+
+const app = express();
+
+const root = {fun:()=> console.log("hello there")}
+
+app.use('/graphql',graphqlHTTP({
+    schema:schema,
+    rootValue:root,
+    graphiql:true
+}))
+
+app.get('/',(req,res)=>{
+    res.send("Up and running graphql")
+});
+
+app.listen(8082,()=>{
+    console.log("Server is running on port 8082")
+    console.log(`http://localhost:8082`)
+});
